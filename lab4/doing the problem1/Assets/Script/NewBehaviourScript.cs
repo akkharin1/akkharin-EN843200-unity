78dd1251b4735c18717dddf943dad5e817d154d8
akkharin1 <69181084+akkharin1@users.noreply.github.com>
2021-10-04 12:14:37 +0700

    s

---
+++ lab4/doing the problem1/Assets/Script/NewBehaviourScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float speed = 0.0f;
+    public float distance = 0.0f;
+    public float time = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpeedCheck();
+        }
+    }
+
+    void SpeedCheck()
+    {
+        speed = distance / time;
+        if (speed > 70 || speed < 40)
+        {
+            print("Yor are breaking the law!");
+        }
+        print("You are traveling at" + speed + "MPH");
+    }
+}
+
